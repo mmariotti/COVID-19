44075de185011c8e0ec924fcb64a4cd36c2da24b
Michele Mariotti <mariotti.shape@gmail.com>
2020-03-16 13:01:28 +0100

    re-added

--- src/it/mmariotti/covid19/controller/Controller.java
+++ src/it/mmariotti/covid19/controller/Controller.java
@@ -1,28 +1,35 @@
 package it.mmariotti.covid19.controller;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.enterprise.context.ApplicationScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
+import org.omnifaces.util.Faces;
+import org.primefaces.context.RequestContext;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.LegendPlacement;
+import org.primefaces.model.chart.LineChartSeries;
 import it.mmariotti.covid19.model.Regione;
 import it.mmariotti.covid19.model.Regione_;
 import it.mmariotti.covid19.service.UpdateService;
 
 
 @Named
-@ApplicationScoped
+@ViewScoped
 public class Controller implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 
-	@PersistenceUnit
-	private EntityManagerFactory emf;
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MMM-dd");
 
-	private String[] properties = {
+	private static final String[] PROPERTIES = {
 		Regione_.tamponi.getName(),
 		Regione_.positiviTotali.getName(),
 		Regione_.positiviAttuali.getName(),
@@ -36,14 +43,69 @@ public class Controller implements Serializable
 	@EJB
 	private UpdateService service;
 
+	private List<Regione> latestData;
+
+	private BarChartModel chartModel;
+
+	@PostConstruct
+	public void init()
+	{
+		latestData = new ArrayList<>(service.getLatestData());
+	}
+
+	public void showChart(Regione regione, String property, boolean percent)
+	{
+		Faces.setRequestAttribute("chartRegione", regione.getDenominazione());
+		Faces.setRequestAttribute("chartProperty", property);
+
+		chartModel = new BarChartModel();
+
+		LineChartSeries deltaSeries = new LineChartSeries();
+		deltaSeries.setLabel("Increment");
+
+		LineChartSeries totalSeries = new LineChartSeries();
+		totalSeries.setLabel("Total");
+
+		if(percent)
+		{
+			Axis yAxis = chartModel.getAxis(AxisType.Y);
+			yAxis.setTickFormat("%.2f%%");
+		}
+
+		List<Regione> data = service.getRegionData(regione.getDenominazione());
+		for(Regione r : data)
+		{
+			String key = DATE_FORMAT.format(r.getData());
+
+			Number total = Faces.resolveExpressionGet(r, property);
+			Number delta = Faces.resolveExpressionGet(r, property + "Delta");
+
+			if(percent)
+			{
+				total = total.doubleValue() * 100;
+				delta = delta.doubleValue() * 100;
+			}
+
+			deltaSeries.set(key, delta);
+			totalSeries.set(key, total);
+		}
+
+		chartModel.addSeries(totalSeries);
+		chartModel.addSeries(deltaSeries);
+
+		chartModel.setLegendPosition("nw");
+		chartModel.setLegendPlacement(LegendPlacement.OUTSIDEGRID);
+		chartModel.setMouseoverHighlight(true);
+		chartModel.setShowDatatip(true);
+		chartModel.setShowPointLabels(true);
+//		chartModel.setStacked(true);
+
+		RequestContext.getCurrentInstance().execute("PF('chartDialog').show()");
+	}
+
 	public Date getLastUpdate()
 	{
 		return service.getLastUpdate();
-	}
-
-	public List<Regione> getLatestData()
-	{
-		return service.getLatestData();
 	}
 
 	public Regione getLastSummary()
@@ -51,8 +113,18 @@ public class Controller implements Serializable
 		return service.getLastSummary();
 	}
 
+	public List<Regione> getLatestData()
+	{
+		return latestData;
+	}
+
 	public String[] getProperties()
 	{
-		return properties;
+		return PROPERTIES;
+	}
+
+	public BarChartModel getChartModel()
+	{
+		return chartModel;
 	}
 }
